414a30d135cf89580f204ea6dc37d5e6d088c193
turbo <petya.petrov.1990@gmail.com>
2018-10-06 17:32:02 +0500

    first commit

---
+++ public/js/slickControll.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+      });
+});
